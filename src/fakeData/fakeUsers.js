ddfdbf7567f4210a01a4a30e7fd6c3c2094e8867
Kasidate444 <kasidatesaeeaw@gmail.com>
2026-05-14 13:23:34 +0700

    Add create, read and update routes for user resource

---
+++ src/fakeData/fakeUsers.js
@@ -0,0 +1,20 @@
+export const users = [
+  {
+    id: "1",
+    username: "Johnny",
+    email: "johnny@example.com",
+    password: "123456789",
+  },
+  {
+    id: "2",
+    username: "Bobb",
+    email: "bobb@example.com",
+    password: "456789123",
+  },
+  {
+    id: "3",
+    username: "Charlie",
+    email: "charlie@example.com",
+    password: "789123456",
+  },
+];
