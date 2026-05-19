0f438886db208193d317749f2491afff5d3f48a1
Kasidate444 <kasidatesaeeaw@gmail.com>
2026-05-19 11:11:12 +0700

    refactor users controllers.

--- src/routes/v2/users.routes.js
+++ src/routes/v2/users.routes.js
@@ -2,67 +2,19 @@ import { Router } from "express";
 // import { users } from "../../fakeData/fakeUsers.js";
 import { User } from "../../modules/users/user.model.js";
 import { supabase } from "../../config/supabase.js";
+import {
+  getUsers,
+  cerateUser,
+  updateUser,
+} from "../../modules/users/users.controller.js";
 
 export const router = Router();
 
-const userResponse = (doc) => {
-  const user = doc.toObject();
-  delete user.password;
-  return user;
-};
+router.get("/", getUsers);
 
-router.get("/", async (req, res) => {
-  try {
-    const user = await User.find();
-    return res.status(200).json({ success: true, data: user });
-  } catch (error) {
-    return res.status(400).json({ success: false, error: error });
-  }
-});
+router.post("/", cerateUser);
 
-router.post("/", async (req, res) => {
-  const { username, email, password, role } = req.body || {};
-  if (!username || !email || !password) {
-    const err = new Error("username, email, and password are required");
-    err.name = "ValidationError";
-    err.status = 400;
-    return res.status(400).json({ sucess: false, error: err });
-  }
-
-  try {
-    const doc = await User.create({
-      username: username,
-      email: email,
-      password: password,
-      role: role,
-    });
-    return res.status(201).json({ success: true, date: userResponse(doc) });
-  } catch (err) {
-    return res.status(400).json({ success: false, error: err });
-  }
-});
-
-router.put("/:id", async (req, res) => {
-  const user = users.find((u) => u.id === req.params.id);
-
-  if (!user) {
-    return res.status(404).json({ error: "User not found!" });
-  }
-
-  const { username, email, password } = req.body;
-
-  if (!username || !email || !password) {
-    return res
-      .status(400)
-      .json({ error: "username, email and password are required!" });
-  }
-
-  user.username = username;
-  user.email = email;
-  user.password = password;
-
-  res.status(200).json(user);
-});
+router.put("/:id", updateUser);
 
 // router.delete("/:id",async (req,res)=>{});
 
@@ -104,3 +56,5 @@ router.post("/pg", async (req, res) => {
     return res.status(400).json({ success: false, error: error.message });
   }
 });
+
+// router.put("/pg/:id")
